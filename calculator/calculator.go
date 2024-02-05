140bf4a03b8b101cf5da49058662b82f255c1e90
Julian Ruiz <jnrt96@gmail.com>
2024-02-05 13:41:53 -0700

    inicio del repositorio

---
+++ calculator/calculator.go
@@ -0,0 +1,24 @@
+package calculator
+
+import "errors"
+
+type Calculator struct{}
+
+func (c *Calculator) Sum(a, b int) int {
+	return a + b
+}
+
+func (c *Calculator) Subtract(a, b int) int {
+	return a - b
+}
+
+func (c *Calculator) Multiply(a, b int) int {
+	return a * b
+}
+
+func (c *Calculator) Divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("cannot divide by zero")
+	}
+	return a / b, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Julianrt/test
+
+go 1.21.1
